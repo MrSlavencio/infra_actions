ace243b28712c8de87b5b86401167e93fd4ad201
MrSlavencio <vyacheslavkobzeff@yandex.ru>
2023-02-05 09:39:53 +0300

    первый комит

--- infra_project/manage.py
+++ infra_project/manage.py
@@ -5,6 +5,8 @@ import sys
 
 
 def main():
+    """Main func."""
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infra_project.settings')
     try:
         from django.core.management import execute_from_command_line
